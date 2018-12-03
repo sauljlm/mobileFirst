6e2139f7c1f28dd04a0332ee1b493a0dc12d6578
Saul Lopez <slopez@LAB-DIC-3.local>
2018-12-03 10:03:43 -0600

    termino responsive

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
-const hamburguer = document.getElementById('btn_menu');
-const menu
+// funcion para el menu
+let btn = false;
+const hamburguer = document.getElementById('hamburguer');
+const menu = document.getElementById('menu');
+function showMenu(){
+    if (btn === false) {
+        menu.style.display = "inline-block";
+        btn = true;
+    }else if (btn === true) {
+        menu.style.display = "none";
+        btn = false;
+    }
+}
+hamburguer.addEventListener('click', showMenu);
+// fin funcion para el menu
